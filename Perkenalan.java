ac90b997942af974c2f1c0e14f4f7b44a754cf43
RaffyJO <rafiteguh6@gmail.com>
2023-11-01 10:20:20 +0700

    kelas

--- Perkenalan.java
+++ Perkenalan.java
@@ -4,7 +4,7 @@ public class Perkenalan {
         System.out.println("raffi");
 
         // Kelas
-        System.out.println();
+        System.out.println("2H");
 
         // Harapan
         System.out.println();
