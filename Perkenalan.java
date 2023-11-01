6588d64a269d6953c23d3a1169099d16a449c3e7
RaffyJO <rafiteguh6@gmail.com>
2023-11-01 10:10:02 +0700

    first commit

---
+++ Perkenalan.java
@@ -0,0 +1,12 @@
+public class Perkenalan {
+    public static void main(String[] args) {
+        // Nama
+        System.out.println();
+
+        // Kelas
+        System.out.println();
+
+        // Harapan
+        System.out.println();
+    }
+}
