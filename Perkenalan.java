608919322dfe85b67ecaaeab5b799771a360ed68
dinaarh <rahmawatidina0404@gmail.com>
2023-11-06 20:55:43 +0700

    intro dina

--- Perkenalan.java
+++ Perkenalan.java
@@ -1,12 +1,15 @@
 public class Perkenalan {
     public static void main(String[] args) {
         // Nama
-        System.out.println("Raffy Jamil Octavialdy");
+        System.out.println("Dina Rahmawati");
 
         // Kelas
-        System.out.println("2H");
+        System.out.println("1G TI");
 
         // Harapan
-        System.out.println("Sukses");
+        System.out.println("Mendapat ilmu baru,relasi banyak");
+
+        System.out.println("Kunci kesehatan cuma 1, yaa jangan sakit");
+
     }
 }
